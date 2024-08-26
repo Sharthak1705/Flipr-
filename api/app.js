aaede0ac3808aae161d2973e0b893eafa82d7034
Sharthak Jain <113213380+Sharthak1705@users.noreply.github.com>
2024-08-27 02:14:36 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const Contact = require('./routes/contact.route');
 const Newsletter = require('./routes/subscription.route');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
